437427b685a86f5f3fe3e98b191946c563fc83a6
Node.js <nodejs@MacBook-di-Jacopo-Col.local>
2014-07-10 20:48:28 +0200

    Fixed selected in the menu, fixed various errors in the server that made the app crash when the search result was false or null

--- app.js
+++ app.js
@@ -28,29 +28,31 @@ function handler (req, res) {
 
 //The function grabs the search term trough ricerca parameter and the client.id trough the id paramenter.
 var stream; //defined here so I cann call it to stop later
+var ricerca; //defined here so I cann call it to stop later 
 var skippabletweets = [];
 
-function streamSearched(ricerca, id) {
-	stream = T.stream('statuses/filter', { track: ricerca })
-		
-	stream.on('connected', function (response) {
-	  io.sockets.in(id).emit('connected');
-	  console.log("CONNESSO");
-	});
-	
-	console.log(id);
-	console.log(ricerca + " tweet");
-		
-		
-		stream.on('tweet', function (tweet) {
-		   		    
-		    io.sockets.in(id).emit('tweet', tweet.text, tweet.user.followers_count, tweet.user.profile_link_color);
+function streamSearched(searchterm, id) {
+
+	if (searchterm == null || searchterm == false) {
+		console.log(searchterm);
+	}
+
+	else {
+		stream = T.stream('statuses/filter', { track: searchterm })
+		stream.on('connected', function (response) {
+		  io.sockets.in(id).emit('connected');
+		  console.log("CONNESSO");
 		});
-		
+		console.log(id);
+		console.log(searchterm + " tweet");
+		stream.on('tweet', function (tweet) {
+		   console.log(tweet.text);		    
+		    io.sockets.in(id).emit('tweet', tweet.text, tweet.user.followers_count, tweet.user.profile_link_color);
+			});
 		stream.on('limit', function (limitMessage) {
-		  console.log('Limit message: '+limitMessage);
-		})
-	
+			  console.log('Limit message: '+limitMessage);
+			})
+	}
 	
 };
 
@@ -59,15 +61,17 @@ function streamSearched(ricerca, id) {
 io.sockets.on('connection', function(client) {
 		client.on('search', function(searchterm) {
 				console.log(searchterm);
-					console.log(client.id);
-						streamSearched(searchterm, client.id);
+					ricerca = searchterm;
+						console.log(client.id);
+							streamSearched(searchterm, client.id);
 		
 		client.on('disconnect', function () {
 			console.log('client disconnected');
-			stream.stop();
-			//Trova un modo di fermare lo stream per lo specifico client
-			//var stream = T.stream();
-			//stream.stop();
+			
+			if (ricerca != null && ricerca != false) {
+			stream.stop();	
+			}
+			
 			});					
 		
 	});
